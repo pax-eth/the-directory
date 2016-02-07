c56d02c62ed82b4c583e801994a4980c3ed7f88e
Philip <philip@pax.directory>
2016-02-07 20:37:10 +0000

    template broadcast input 0.1

--- directory/scripts/broadcasts/out.js
+++ directory/scripts/broadcasts/out.js
@@ -1,15 +1,14 @@
 
-
-<!-- Client is a JSON object that includes status -->
+/*Client is a JSON object that includes status */
 
 var out = function() {
 		document.getElementById('out')
-	}
+	};
 
 if client.status <= 0.5 {
 	template.	
 
-}
+};
 
 <template name="out">
 <div class="panel panel-default">
@@ -19,10 +18,24 @@ if client.status <= 0.5 {
 <div class="form-group">
 <textarea class="form-control" id="broadcast"></textarea>
 
-<div class="panel-footer" id="out_footer"></div>
+<div class="panel-footer" id="out_footer">
 
+/*Input button with public and private options*/
+<div class="btn-group">
+  <button type="button" class="btn btn-default">Broadcast</button>
+  <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown">
+    <span class="caret"></span>
+  </button>
+  <ul class="dropdown-menu" role="menu">
+    <li><a href="#">Public</a></li>
+    <li><a href="#">Private</a></li>
+  </ul>
+</div>
+</div>
 </form>
+/*End of form*/
 </div>
 </div>
+/*End of panel */
 </template>
 
