961f5a8340c007856860ad87ef494d77bd8f334a
Philip <philip@pax.directory>
2016-02-07 17:09:09 +0000

    sketch broadcast template, add broadcast logs

---
+++ directory/scripts/broadcasts/out.js
@@ -0,0 +1,28 @@
+
+
+<!-- Client is a JSON object that includes status -->
+
+var out = function() {
+		document.getElementById('out')
+	}
+
+if client.status <= 0.5 {
+	template.	
+
+}
+
+<template name="out">
+<div class="panel panel-default">
+<div class="panel-heading" id="out_header"></div>
+<div class="panel-body" id="out_body">
+<form role="form">
+<div class="form-group">
+<textarea class="form-control" id="broadcast"></textarea>
+
+<div class="panel-footer" id="out_footer"></div>
+
+</form>
+</div>
+</div>
+</template>
+
